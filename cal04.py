cc9845a11fc90f46ae0fcae3763466f9b49b0796
Areeba456 <areebar2019@namal.edu.pk>
2021-10-02 19:41:21 +0500

    div function

---
+++ cal04.py
@@ -0,0 +1,19 @@
+# calculator in Python(v0.4)
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2)
+	Return num1 - num2
+
+def mul(num1, num2)
+	return num1 * num2
+
+def div(num1, num2)
+	return num1 / num2
+
+# main
+res = add(5, 3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
