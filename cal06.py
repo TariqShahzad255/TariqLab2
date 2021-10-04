89409f74d1992797e2061539a0a63ff8abf52fd9
Asad3290 <asad2019@namal.edu.pk>
2021-10-04 09:02:44 -0700

    square functionality

---
+++ cal06.py
@@ -0,0 +1,28 @@
+# calculator in Python(v0.4)
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2):
+	return num1 - num2
+
+def mul(num1, num2):
+	return num1 * num2
+
+def div(num1, num2):
+	return num1 / num2
+
+def sqr(number):
+	return number ** 0.5
+
+def square(num1, num2):
+	return num1 ** num2
+
+# main
+number = int(input("Enter a number: "))
+res = add(5, 3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+print("The square root of", number, "is", sqr(number))
+print(square(5,2))
